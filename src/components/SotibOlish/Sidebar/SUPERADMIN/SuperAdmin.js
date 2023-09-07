8af7db53a58e4dba66af409749da25ca9ba65e80
JovliyevOlim <olimjovliyev98@gmail.com>
2023-09-07 12:18:46 +0500

    superadmin delete

--- src/components/SotibOlish/Sidebar/SUPERADMIN/SuperAdmin.js
+++ src/components/SotibOlish/Sidebar/SUPERADMIN/SuperAdmin.js
@@ -5,11 +5,8 @@ const SuperAdminPages = lazy(() => import('./pages/superadmin/SuperAdminPage'))
 const AllBusenesses = lazy(() => import('./pages/all-buseness/AllBusenesses'))
 const PackageSubscription = lazy(() => import('./pages/package-subscription/PackageSubscripton'))
 const Packages = lazy(() => import('./pages/packages/Packages'))
-const SuperAdminSettings = lazy(() => import('./pages/super-admin-settings/SuperAdminSettings'))
-const Communicator = lazy(() => import('./pages/communicator/Communicator'))
 const AddBusiness = lazy(() => import('./pages/all-buseness/addBusiness/AddBusiness'))
 const PackageAdd = lazy(() => import('./pages/packages/PackageAdd/PackageAdd'))
-const MainPageTitles = lazy(() => import("./pages/main-pages-titles/MainPageTitles"))
 
 function SuperAdmin() {
 
@@ -19,9 +16,6 @@ function SuperAdmin() {
             <Link to='/main/superadmin/allbusenesses'> <button>All Busenesses</button></Link>
             <Link to='/main/superadmin/packagesubscription'> <button>Package Subscription</button></Link>
             <Link to='/main/superadmin/paskages'> <button>Paskages</button></Link>
-            {/*<Link to='/superadmin/titles'> <button>Titles</button></Link>*/}
-            {/*<Link to='/superadmin/superadminsettings'> <button>Super Admin Settings</button></Link>*/}
-            {/*<Link to='/superadmin/changer'> <button>Change Users Password</button></Link>*/}
         </div>
         <div className='mt-2'>
             <Switch>
@@ -32,9 +26,6 @@ function SuperAdmin() {
                 <Route path={'/main/superadmin/paskages/addpackage/:id'} component={PackageAdd}/>
                 <Route path={'/main/superadmin/paskages/addpackage'} component={PackageAdd}/>
                 <Route path={'/main/superadmin/paskages'} component={Packages}/>
-                {/*<Route path={'/superadmin/titles'} component={MainPageTitles}/>*/}
-                {/*<Route path={'/superadmin/superadminsettings'} component={SuperAdminSettings}/>*/}
-                {/*<Route path={'/superadmin/changer'} component={Communicator}/>*/}
                 <Route path={'/main/superadmin'} component={SuperAdminPages}/>
             </Switch>
         </div>
